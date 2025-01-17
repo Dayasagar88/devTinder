cede8ff805d64fbfecee3fa61fb5f3a06d121119
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2025-01-17 12:25:23 +0530

    delete , update user api

--- src/app.js
+++ src/app.js
@@ -16,6 +16,85 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  if (!userEmail) {
+    return res.send("Email Id required");
+  }
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+    console.log(error);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length > 0) {
+      res.status(200).json({ message: `${users.length} users found`, users });
+    } else {
+      res.status(404).send("No users found");
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+    console.log("Error : ", error);
+  }
+});
+
+app.delete("/delete-user/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    // Find and delete the user in one step
+    const deletedUser = await User.findByIdAndDelete(userId);
+    console.log(deletedUser);
+
+    // Check if a user was deleted
+    if (!deletedUser) {
+      return res.status(404).send("User not found");
+    }
+
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.patch("/update-user/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    // Find and update the user in one step
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      firstName: "jassi",
+      lastName: "gill",
+      age : 24,
+      gender : "Male"
+    }, {returnDocument : "after"});
+    console.log(updatedUser);
+
+    // Check if a user was deleted
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    }
+
+    res.status(200).send("User updated successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established!😃");
